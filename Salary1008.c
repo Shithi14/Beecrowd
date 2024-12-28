0f161880c4219d285010eab9618fcaa27bb45c82
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 15:26:31 +0600

    Add files via upload

---
+++ Salary1008.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    float c,total;
+    scanf("%d%d%f",&a,&b,&c);
+
+    total=(b*c);
+    printf("NUMBER = %d\n",a);
+    printf("SALARY = U$ %.2f\n",total);
+    return 0;
+    
+}
